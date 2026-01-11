e15c8302dc397e7e1f9db908fa229bf8d6139174
baihu <baihu3210@gmail.com>
2026-01-11 18:35:01 +0800

    feat: 添加 Gyro 實例

---
+++ src/main/java/frc/robot/Subsystem/Gyro/Gyro.java
@@ -0,0 +1,29 @@
+package frc.robot.Subsystem.Gyro;
+
+import com.studica.frc.AHRS;
+import com.studica.frc.AHRS.NavXComType;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+
+public class Gyro {
+    public static Gyro gyro;
+
+    private final AHRS ahrs;
+
+    public Gyro() {
+        ahrs = new AHRS(NavXComType.kMXP_SPI);
+    }
+
+    public Rotation2d getRotation() {
+        return ahrs.getRotation2d().unaryMinus();
+    }
+
+    public void reset() {
+        ahrs.reset();
+    }
+
+    public static Gyro getInstance() {
+        if (gyro == null) gyro = new Gyro();
+        return gyro;
+    }
+}
