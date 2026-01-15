437753a041438d283191db532d5c9e40f9e6c213
baihu <baihu3210@gmail.com>
2026-01-15 21:02:48 +0800

    feat: refactor DrivetrainConfigs to use methods for driving and turning configurations

--- src/main/java/frc/robot/Subsystem/Drivetrain/DrivetrainConfigs.java
+++ src/main/java/frc/robot/Subsystem/Drivetrain/DrivetrainConfigs.java
@@ -3,20 +3,18 @@ package frc.robot.Subsystem.Drivetrain;
 import frc.robot.Infrastructure.SparkMaxConfigFactory;
 
 public class DrivetrainConfigs {
-    public static final SparkMaxConfigFactory drivingConfig;
-    public static final SparkMaxConfigFactory turningConfig;
-
-    static {
-        drivingConfig = new SparkMaxConfigFactory(44, false);
-        turningConfig = new SparkMaxConfigFactory(20, false);
-
+    public static SparkMaxConfigFactory drivingConfig() {
+        SparkMaxConfigFactory drivingConfig = new SparkMaxConfigFactory(44, false);
         drivingConfig.setEncoder(Constants.PositionConversionFactor, Constants.VelocityConversionFactor);
-        turningConfig.setAbsoluteEncoder(Constants.TurnPositionConversionFactor, Constants.VelocityConversionFactor, false);
-
         drivingConfig.setPID(0.04, 0, 0, 1 / Constants.maxSpeed);
-        turningConfig.setPID(1.0, 0, 0);
-
-        turningConfig.setpositionWrapping(-Math.PI, Math.PI);
+        return drivingConfig;
     }
 
+    public static SparkMaxConfigFactory turningConfig(double angleOffset) {
+        SparkMaxConfigFactory turningConfig = new SparkMaxConfigFactory(20, false);
+        turningConfig.setAbsoluteEncoder(Constants.TurnPositionConversionFactor, Constants.VelocityConversionFactor, angleOffset, false);
+        turningConfig.setPID(1.0, 0, 0);
+        turningConfig.setPositionWrapping(-Math.PI, Math.PI);
+        return turningConfig;
+    }
 }
