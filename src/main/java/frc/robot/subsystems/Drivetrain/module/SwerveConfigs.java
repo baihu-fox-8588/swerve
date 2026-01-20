360230af63fb7a53f3916637803a43eff0bf9c78
baihu <baihu3210@gmail.com>
2026-01-20 16:27:40 +0800

    fix: fix file name

--- src/main/java/frc/robot/subsystems/Drivetrain/module/SwerveConfigs.java
+++ src/main/java/frc/robot/subsystems/Drivetrain/module/SwerveConfigs.java
@@ -1,19 +1,19 @@
 package frc.robot.subsystems.Drivetrain.module;
 
-import frc.robot.constants.drivetrainConstants;
+import frc.robot.constants.DrivetrainConstants;
 import frc.robot.utils.SparkMaxConfiguration;
 
 public class SwerveConfigs {
     public static SparkMaxConfiguration drivingConfig() {
         SparkMaxConfiguration drivingConfig = new SparkMaxConfiguration(44, false);
-        drivingConfig.setEncoder(drivetrainConstants.PositionConversionFactor, drivetrainConstants.VelocityConversionFactor);
-        drivingConfig.setPID(0.04, 0, 0, 1 / drivetrainConstants.maxSpeed);
+        drivingConfig.setEncoder(DrivetrainConstants.PositionConversionFactor, DrivetrainConstants.VelocityConversionFactor);
+        drivingConfig.setPID(0.04, 0, 0, 1 / DrivetrainConstants.maxSpeed);
         return drivingConfig;
     }
 
     public static SparkMaxConfiguration turningConfig(double angleOffset) {
         SparkMaxConfiguration turningConfig = new SparkMaxConfiguration(20, false);
-        turningConfig.setAbsoluteEncoder(drivetrainConstants.TurnPositionConversionFactor, drivetrainConstants.VelocityConversionFactor, angleOffset, false);
+        turningConfig.setAbsoluteEncoder(DrivetrainConstants.TurnPositionConversionFactor, DrivetrainConstants.VelocityConversionFactor, angleOffset, false);
         turningConfig.setPID(1.0, 0, 0);
         turningConfig.setPositionWrapping(-Math.PI, Math.PI);
         return turningConfig;
