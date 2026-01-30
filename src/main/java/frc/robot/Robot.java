1ce42ae36905f36473f3a81c83a2b7bf675b60f4
baihu <baihu3210@gmail.com>
2026-01-30 09:11:49 +0800

    fix: format code in Robot.java for improved readability and consistency

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,55 +5,55 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
-  private Command autonomousCommand;
-  private final RobotContainer robotContainer;
+  	private Command autonomousCommand;
+  	private final RobotContainer robotContainer;
 
-  public Robot() {
-    robotContainer = new RobotContainer();
-  }
+  	public Robot() {
+    	robotContainer = new RobotContainer();
+  	}
 
  	public void robotInit() {
   	}
 
   	@Override
-  	public void robotPeriodic() {
-    	CommandScheduler.getInstance().run();
-  }
+		public void robotPeriodic() {
+  		CommandScheduler.getInstance().run();
+  	}
 
-  @Override
-  public void disabledInit() {}
+  	@Override
+  	public void disabledInit() {}
 
-  @Override
-  public void disabledPeriodic() {}
+  	@Override
+  	public void disabledPeriodic() {}
 
-  @Override
-  public void disabledExit() {}
+  	@Override
+  	public void disabledExit() {}
 
-  @Override
-  public void autonomousInit() {
+  	@Override
+  	public void autonomousInit() {
     autonomousCommand = robotContainer.getAutonomousCommand();
 
     if (autonomousCommand != null) {
-      autonomousCommand.schedule();
-    }
-  }
+      		autonomousCommand.schedule();
+    	}
+  	}
 
-  @Override
-  public void autonomousPeriodic() {}
+  	@Override
+  	public void autonomousPeriodic() {}
 
-  @Override
-  public void autonomousExit() {}
+  	@Override
+  	public void autonomousExit() {}
 
-  @Override
-  public void teleopInit() {
-    if (autonomousCommand != null) {
-      autonomousCommand.cancel();
-    }
-  }
+  	@Override
+  	public void teleopInit() {
+    	if (autonomousCommand != null) {
+      		autonomousCommand.cancel();
+    	}
+  	}
 
-  @Override
-  public void teleopPeriodic() {}
+  	@Override
+  	public void teleopPeriodic() {}
 
-  @Override
-  public void teleopExit() {}
+  	@Override
+ 	public void teleopExit() {}
 }
